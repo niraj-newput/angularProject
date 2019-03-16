de0198d7bbdae70819458506d71139203a382291
Niraj <nirajpatidar11@gmail.com>
2019-03-16 17:34:33 +0530

    changes in server

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express'),
 path = require('path');
 
 const app = express();
-app.use(express.static('./dist/angular-project'));
+app.use(express.static('./angular-project'));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/angular-project/index.html'));
+  res.sendFile(path.join(__dirname, '/angular-project/index.html'));
 });
 app.listen(process.env.PORT || 8080, () => {
   console.log('server started');
