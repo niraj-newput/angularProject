20e90ddfd01afe992606ede8dd4300f1b6ccb7d0
Niraj <nirajpatidar11@gmail.com>
2019-03-16 18:51:53 +0530

    change

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express'),
 path = require('path');
 
 const app = express();
-
+app.use(expess.static(__dirname + '/dist'));
 app.get('/*', (req, res) => {
   console.log(__dirname)
-  res.sendFile(path.join(__dirname ,'/src/index.html'));
+  res.sendFile(path.join(__dirname ,'/dist/index.html'));
 });
 app.listen(process.env.PORT || 8080, () => {
   console.log('server started');
-
+console.log(path.join(__dirname ,'/src/index.html'))
 })
