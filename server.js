429a2857180abcafd4b503f21ec40346c9304875
Niraj <nirajpatidar11@gmail.com>
2019-03-16 17:43:01 +0530

    changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ path = require('path');
 const app = express();
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.listen(process.env.PORT || 8080, () => {
   console.log('server started');
