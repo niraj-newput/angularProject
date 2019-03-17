78f56aa1d72aaaab4e9552be1739dd190a90b526
Niraj <nirajpatidar11@gmail.com>
2019-03-17 19:46:56 +0530

    create side bar and top nav

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ path = require('path');
 const app = express();
 app.use(express.static(__dirname,'/dist/angularProject'));
 app.get('*', (req, res) => {
-  console.log(__dirname)
   res.sendFile(path.join(__dirname, '/dist/angularProject/index.html'));
 });
 app.listen(process.env.PORT || 8080, () => {
   console.log('server started');
-console.log(path.join(__dirname ,'/dist/angularProject/index.html'))
+console.log(path.join(__dirname ,'/dist/angularProject/index.html'));
 })
